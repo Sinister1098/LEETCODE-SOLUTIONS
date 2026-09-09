fdef1ccd056814a32757d5480b49ff7088c08ec8
Harshvardhan Tyagi <tyagih199@gmail.com>
2026-09-09 23:16:57 +0530

    Time: 0 ms (100%), Space: 14.8 MB (98.16%) - LeetHub

---
+++ 0086-partition-list/0086-partition-list.cpp
@@ -0,0 +1,29 @@
+
+class Solution {
+public:
+    ListNode* partition(ListNode* head, int x) {
+        ListNode* lo = new ListNode(100);
+        ListNode* hi = new ListNode(254);
+        ListNode* templo=lo;
+        ListNode* temphi=hi;
+        ListNode* temp = head;
+        while(temp!=NULL){
+            if(temp->val<x){
+                templo->next=temp;
+                temp=temp->next;
+                templo=templo->next;
+            }
+            else{
+                temphi->next=temp;
+                temp=temp->next;
+                temphi=temphi->next;
+
+            }
+        }
+        templo->next=hi->next;
+        temphi->next=NULL;
+        return lo->next;
+        
+
+    }
+};
